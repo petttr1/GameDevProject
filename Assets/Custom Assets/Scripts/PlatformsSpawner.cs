7306f973157e012bdc2c2c7e9175c11d849ac892
Peter Bokor <petttr1@gmail.com>
2020-10-10 18:31:57 +0200

    scratched the character, added a new one, started working on platforms generation

---
+++ Assets/Custom Assets/Scripts/PlatformsSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformsSpawner : MonoBehaviour
+{
+
+    public int amount = 10;
+    public bool isGameStart = true;
+    public Vector3 center;
+    public GameObject platform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isGameStart == true){
+            isGameStart = false;
+            Instantiate(platform, center, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
